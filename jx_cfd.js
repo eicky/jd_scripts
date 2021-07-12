376d68c9a9d81262be399f7a69c277caedc317a2
yuannian1112 <84368132+yuannian1112@users.noreply.github.com>
2021-07-12 14:51:19 +0800

    Update jx_cfd.js

--- jx_cfd.js
+++ jx_cfd.js
@@ -6,7 +6,6 @@
  * 领奖、任务
  cron 5 * * * * https://raw.githubusercontent.com/yuannian1112/jd_scripts/main/jx_cfd.js
  */
-const $ = new Env('财富岛');
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
