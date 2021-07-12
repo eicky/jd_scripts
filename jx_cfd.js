94d42dee1c3cca49f0708419ebf97013b5a8ed8b
yuannian1112 <84368132+yuannian1112@users.noreply.github.com>
2021-07-12 22:03:43 +0800

    Update jx_cfd.js

--- jx_cfd.js
+++ jx_cfd.js
@@ -1,11 +1,10 @@
 "use strict";
 /**
- * 京喜牧场
- * 买、喂、收蛋、锄草、挑逗
- * // TODO
- * 领奖、任务
+ * 京喜财富岛
+ * 包含雇佣导游，建议每小时1次
  cron 5 * * * * https://raw.githubusercontent.com/yuannian1112/jd_scripts/main/jx_cfd.js
  */
+"use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -47,25 +46,23 @@ var date_fns_1 = require("date-fns");
 var axios_1 = require("axios");
 var TS_USER_AGENTS_1 = require("./TS_USER_AGENTS");
 var CryptoJS = require('crypto-js');
-// console.log('时间戳：', format(new Date(), 'yyyyMMddHHmmssSSS'));
 var appId = 10028, fingerprint, token, enCryptMethodJD;
 var cookie = '', cookiesArr = [], res = '', shareCodes;
-var homePageInfo;
 var UserName, index, isLogin, nickName;
 !(function () { return __awaiter(void 0, void 0, void 0, function () {
-    var i, tasks, _i, _a, t, _b, _c, b;
-    return __generator(this, function (_d) {
-        switch (_d.label) {
+    var i, tasks, _i, _a, e, employ, _b, _c, t, _d, _e, b;
+    return __generator(this, function (_f) {
+        switch (_f.label) {
             case 0: return [4 /*yield*/, requestAlgo()];
             case 1:
-                _d.sent();
+                _f.sent();
                 return [4 /*yield*/, requireConfig()];
             case 2:
-                _d.sent();
+                _f.sent();
                 i = 0;
-                _d.label = 3;
+                _f.label = 3;
             case 3:
-                if (!(i < cookiesArr.length)) return [3 /*break*/, 27];
+                if (!(i < cookiesArr.length)) return [3 /*break*/, 30];
                 cookie = cookiesArr[i];
                 UserName = decodeURIComponent(cookie.match(/pt_pin=([^;]*)/)[1]);
                 index = i + 1;
@@ -73,28 +70,11 @@ var UserName, index, isLogin, nickName;
                 nickName = '';
                 return [4 /*yield*/, TotalBean()];
             case 4:
-                _d.sent();
+                _f.sent();
                 console.log("\n\u5F00\u59CB\u3010\u4EAC\u4E1C\u8D26\u53F7" + index + "\u3011" + (nickName || UserName) + "\n");
-                i = 0;
-                _d.label = 5;
-            case 5:
-                if (!(i < 20)) return [3 /*break*/, 9];
-                return [4 /*yield*/, speedUp('_cfd_t,bizCode,dwEnv,ptag,source,strBuildIndex,strZone')];
-            case 6:
-                res = _d.sent();
-                console.log(res);
-                console.log('今日热气球:', res.dwTodaySpeedPeople, '/', 20);
-                return [4 /*yield*/, wait(2000)];
-            case 7:
-                _d.sent();
-                _d.label = 8;
-            case 8:
-                i++;
-                return [3 /*break*/, 5];
-            case 9:
                 tasks = void 0;
-                return [4 /*yield*/, mainTask('GetUserTaskStatusList', '_cfd_t,bizCode,dwEnv,ptag,source,strZone,taskId', { taskId: 0 })];
-            case 10:
+                return [4 /*yield*/, api('user/EmployTourGuideInfo', '_cfd_t,bizCode,dwEnv,ptag,source,strZone')];
+            case 5:
                 /*
                  tasks= await api('story/GetActTask', '_cfd_t,bizCode,dwEnv,ptag,source,strZone')
                 for (let t of tasks.Data.TaskList) {
@@ -107,120 +87,112 @@ var UserName, index, isLogin, nickName;
                   }
                 }
                  */
-                tasks = _d.sent();
-                _i = 0, _a = tasks.data.userTaskStatusList;
-                _d.label = 11;
+                // res = await api('story/SpecialUserOper',
+                //   '_cfd_t,bizCode,ddwTriggerDay,dwEnv,dwType,ptag,source,strStoryId,strZone,triggerType',
+                //   {strStoryId: 'stroy_1626065998453014_1', dwType: '2', triggerType: 0, ddwTriggerDay: 1626019200})
+                // console.log('船到:', res)
+                // await wait(31000)
+                // res = await api('story/SpecialUserOper',
+                //   '_cfd_t,bizCode,ddwTriggerDay,dwEnv,dwType,ptag,source,strStoryId,strZone,triggerType',
+                //   {strStoryId: 'stroy_1626065998453014_1', dwType: '3', triggerType: 0, ddwTriggerDay: 1626019200})
+                // console.log('下船:', res)
+                // 导游
+                res = _f.sent();
+                _i = 0, _a = res.TourGuideList;
+                _f.label = 6;
+            case 6:
+                if (!(_i < _a.length)) return [3 /*break*/, 10];
+                e = _a[_i];
+                console.log(e.dwIsUnLock, e.strGuideName);
+                if (!(e.strBuildIndex !== 'food' && e.ddwRemainTm === 0)) return [3 /*break*/, 9];
+                return [4 /*yield*/, api('user/EmployTourGuide', '_cfd_t,bizCode,ddwConsumeCoin,dwEnv,dwIsFree,ptag,source,strBuildIndex,strZone', { ddwConsumeCoin: e.ddwCostCoin, dwIsFree: 0, strBuildIndex: e.strBuildIndex })];
+            case 7:
+                employ = _f.sent();
+                console.log(employ);
+                return [4 /*yield*/, wait(3000)];
+            case 8:
+                _f.sent();
+                _f.label = 9;
+            case 9:
+                _i++;
+                return [3 /*break*/, 6];
+            case 10: return [4 /*yield*/, mainTask('GetUserTaskStatusList', '_cfd_t,bizCode,dwEnv,ptag,source,strZone,taskId', { taskId: 0 })];
             case 11:
-                if (!(_i < _a.length)) return [3 /*break*/, 18];
-                t = _a[_i];
-                if (!(t.dateType === 2)) return [3 /*break*/, 17];
-                if (!(t.awardStatus === 2 && t.completedTimes === t.targetTimes)) return [3 /*break*/, 14];
+                tasks = _f.sent();
+                _b = 0, _c = tasks.data.userTaskStatusList;
+                _f.label = 12;
+            case 12:
+                if (!(_b < _c.length)) return [3 /*break*/, 19];
+                t = _c[_b];
+                if (!(t.dateType === 2)) return [3 /*break*/, 18];
+                if (!(t.awardStatus === 2 && t.completedTimes === t.targetTimes)) return [3 /*break*/, 15];
                 console.log(1, t.taskName);
                 return [4 /*yield*/, mainTask('Award', '_cfd_t,bizCode,dwEnv,ptag,source,strZone,taskId', { taskId: t.taskId })];
-            case 12:
-                res = _d.sent();
+            case 13:
+                res = _f.sent();
                 console.log(res);
                 if (res.ret === 0) {
                     console.log(t.taskName + "\u9886\u5956\u6210\u529F:", res.data.prizeInfo);
                 }
                 return [4 /*yield*/, wait(2000)];
-            case 13:
-                _d.sent();
-                return [3 /*break*/, 17];
             case 14:
-                if (!(t.awardStatus === 2 && t.completedTimes < t.targetTimes && (t.orderId === 2 || t.orderId === 3))) return [3 /*break*/, 17];
-                return [4 /*yield*/, mainTask('DoTask', '_cfd_t,bizCode,configExtra,dwEnv,ptag,source,strZone,taskId', { taskId: t.taskId, configExtra: '' })];
+                _f.sent();
+                return [3 /*break*/, 18];
             case 15:
+                if (!(t.awardStatus === 2 && t.completedTimes < t.targetTimes && (t.orderId === 2 || t.orderId === 3))) return [3 /*break*/, 18];
+                return [4 /*yield*/, mainTask('DoTask', '_cfd_t,bizCode,configExtra,dwEnv,ptag,source,strZone,taskId', { taskId: t.taskId, configExtra: '' })];
+            case 16:
                 // console.log('做任务:', t.taskId, t.taskName, t.completedTimes, t.targetTimes)
-                res = _d.sent();
+                res = _f.sent();
                 console.log('做任务:', res);
                 return [4 /*yield*/, wait(5000)];
-            case 16:
-                _d.sent();
-                _d.label = 17;
             case 17:
-                _i++;
-                return [3 /*break*/, 11];
+                _f.sent();
+                _f.label = 18;
             case 18:
-                _b = 0, _c = ['food', 'fun', 'shop', 'sea'];
-                _d.label = 19;
+                _b++;
+                return [3 /*break*/, 12];
             case 19:
-                if (!(_b < _c.length)) return [3 /*break*/, 25];
-                b = _c[_b];
-                return [4 /*yield*/, api('user/GetBuildInfo', '_cfd_t,bizCode,dwEnv,dwType,ptag,source,strBuildIndex,strZone', { strBuildIndex: b })];
+                _d = 0, _e = ['food', 'fun', 'shop', 'sea'];
+                _f.label = 20;
             case 20:
-                res = _d.sent();
-                console.log(b + "\u5347\u7EA7\u9700\u8981:", res.ddwNextLvlCostCoin);
-                return [4 /*yield*/, wait(1000)
-                    // if (res.dwCanLvlUp === 1) {
-                    //   res = await api('user/BuildLvlUp', '_cfd_t,bizCode,ddwCostCoin,dwEnv,ptag,source,strBuildIndex,strZone', {ddwCostCoin: res.ddwNextLvlCostCoin, strBuildIndex: b})
-                    // if (res.iRet === 0) {
-                    //   console.log(`升级成功`)
-                    //   await wait(2000)
-                    // }
-                    // }
-                ];
+                if (!(_d < _e.length)) return [3 /*break*/, 29];
+                b = _e[_d];
+                return [4 /*yield*/, api('user/GetBuildInfo', '_cfd_t,bizCode,dwEnv,dwType,ptag,source,strBuildIndex,strZone', { strBuildIndex: b })];
             case 21:
-                _d.sent();
-                return [4 /*yield*/, api('user/CollectCoin', '_cfd_t,bizCode,dwEnv,dwType,ptag,source,strBuildIndex,strZone', { strBuildIndex: b, dwType: '1' })];
+                res = _f.sent();
+                console.log(b + "\u5347\u7EA7\u9700\u8981:", res.ddwNextLvlCostCoin);
+                return [4 /*yield*/, wait(1000)];
             case 22:
-                // if (res.dwCanLvlUp === 1) {
-                //   res = await api('user/BuildLvlUp', '_cfd_t,bizCode,ddwCostCoin,dwEnv,ptag,source,strBuildIndex,strZone', {ddwCostCoin: res.ddwNextLvlCostCoin, strBuildIndex: b})
-                // if (res.iRet === 0) {
-                //   console.log(`升级成功`)
-                //   await wait(2000)
-                // }
-                // }
-                res = _d.sent();
+                _f.sent();
+                if (!(res.dwCanLvlUp === 1)) return [3 /*break*/, 25];
+                return [4 /*yield*/, api('user/BuildLvlUp', '_cfd_t,bizCode,ddwCostCoin,dwEnv,ptag,source,strBuildIndex,strZone', { ddwCostCoin: res.ddwNextLvlCostCoin, strBuildIndex: b })];
+            case 23:
+                res = _f.sent();
+                if (!(res.iRet === 0)) return [3 /*break*/, 25];
+                console.log("\u5347\u7EA7\u6210\u529F");
+                return [4 /*yield*/, wait(2000)];
+            case 24:
+                _f.sent();
+                _f.label = 25;
+            case 25: return [4 /*yield*/, api('user/CollectCoin', '_cfd_t,bizCode,dwEnv,dwType,ptag,source,strBuildIndex,strZone', { strBuildIndex: b, dwType: '1' })];
+            case 26:
+                res = _f.sent();
                 console.log(b + "\u6536\u91D1\u5E01:", res.ddwCoin);
                 return [4 /*yield*/, wait(1000)];
-            case 23:
-                _d.sent();
-                _d.label = 24;
-            case 24:
-                _b++;
-                return [3 /*break*/, 19];
-            case 25: return [3 /*break*/, 27];
-            case 26:
+            case 27:
+                _f.sent();
+                _f.label = 28;
+            case 28:
+                _d++;
+                return [3 /*break*/, 20];
+            case 29:
                 i++;
                 return [3 /*break*/, 3];
-            case 27: return [2 /*return*/];
+            case 30: return [2 /*return*/];
         }
     });
 }); })();
-function speedUp(stk, params) {
-    var _this = this;
-    if (params === void 0) { params = {}; }
-    return new Promise(function (resolve) { return __awaiter(_this, void 0, void 0, function () {
-        var url, key, data;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0:
-                    url = "https://m.jingxi.com/jxbfd/user/SpeedUp?strZone=jxbfd&bizCode=jxbfd&source=jxbfd&dwEnv=7&_cfd_t=" + Date.now() + "&ptag=&strBuildIndex=" + ['food', 'shop', 'sea', 'fun'][Math.floor(Math.random() * 4)] + "&_ste=1&_=" + Date.now() + "&sceneval=2&_stk=" + encodeURIComponent(stk);
-                    if (Object.keys(params).length !== 0) {
-                        key = void 0;
-                        for (key in params) {
-                            if (params.hasOwnProperty(key))
-                                url += "&" + key + "=" + params[key];
-                        }
-                    }
-                    url += '&h5st=' + decrypt(stk, url);
-                    return [4 /*yield*/, axios_1["default"].get(url, {
-                            headers: {
-                                'Host': 'm.jingxi.com',
-                                'Referer': 'https://st.jingxi.com/',
-                                'User-Agent': 'jdpingou;android;4.11.0;10;b21fede89fb4bc77;network/wifi;model/M2004J7AC;appBuild/17304;partner/xiaomi;;session/535;aid/b21fede89fb4bc77;oaid/dcb5f3e835497cc3;pap/JA2019_3111789;brand/Xiaomi;eu/8313831616035373;fv/7333732616631643;Mozilla/5.0 (Linux; Android 10; M2004J7AC Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/90.0.4430.91 Mobile Safari/537.36',
-                                'Cookie': cookie
-                            }
-                        })];
-                case 1:
-                    data = (_a.sent()).data;
-                    resolve(data);
-                    return [2 /*return*/];
-            }
-        });
-    }); });
-}
 function api(fn, stk, params) {
     var _this = this;
     if (params === void 0) { params = {}; }
@@ -246,7 +218,7 @@ function api(fn, stk, params) {
                             headers: {
                                 'Host': 'm.jingxi.com',
                                 'Referer': 'https://st.jingxi.com/',
-                                'User-Agent': 'jdpingou;android;4.11.0;10;b21fede89fb4bc77;network/wifi;model/M2004J7AC;appBuild/17304;partner/xiaomi;;session/535;aid/b21fede89fb4bc77;oaid/dcb5f3e835497cc3;pap/JA2019_3111789;brand/Xiaomi;eu/8313831616035373;fv/7333732616631643;Mozilla/5.0 (Linux; Android 10; M2004J7AC Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/90.0.4430.91 Mobile Safari/537.36',
+                                'User-Agent': TS_USER_AGENTS_1["default"],
                                 'Cookie': cookie
                             }
                         })];
@@ -277,17 +249,10 @@ function mainTask(fn, stk, params) {
                     url += '&h5st=' + decrypt(stk, url);
                     return [4 /*yield*/, axios_1["default"].get(url, {
                             headers: {
-                                'Sec-Fetch-Dest': 'script',
-                                'X-Proxyman-Repeated-ID': '09920498',
-                                'Accept': '*/*',
-                                'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
                                 'X-Requested-With': 'com.jd.pingou',
                                 'Referer': 'https://st.jingxi.com/',
                                 'Host': 'm.jingxi.com',
-                                'User-Agent': 'jdpingou;android;4.11.0;10;b21fede89fb4bc77;network/wifi;model/M2004J7AC;appBuild/17304;partner/xiaomi;;session/535;aid/b21fede89fb4bc77;oaid/dcb5f3e835497cc3;pap/JA2019_3111789;brand/Xiaomi;eu/8313831616035373;fv/7333732616631643;Mozilla/5.0 (Linux; Android 10; M2004J7AC Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/90.0.4430.91 Mobile Safari/537.36',
-                                'Sec-Fetch-Site': 'same-site',
-                                'Connection': 'close',
-                                'Sec-Fetch-Mode': 'no-cors',
+                                'User-Agent': TS_USER_AGENTS_1["default"],
                                 'Cookie': cookie
                             }
                         })];
