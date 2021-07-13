ae4987385818774179a8973d985115ea58d0db78
yuannian1112 <84368132+yuannian1112@users.noreply.github.com>
2021-07-13 08:44:25 +0800

    Update jd_summer_movement_help.js

--- jd_summer_movement_help.js
+++ jd_summer_movement_help.js
@@ -18,13 +18,13 @@ if ($.isNode() && process.env.summer_movement_ShHelpFlag) {
   summer_movement_ShHelpFlag = process.env.summer_movement_ShHelpFlag;
 }
 
-const ShHelpAuthorFlag = false;//是否助力作者SH  true 助力，false 不助力
+const ShHelpAuthorFlag = true;//是否助力作者SH  true 助力，false 不助力
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [];
 $.cookie = '';
 $.secretpInfo = {};
 $.ShInviteList = [];
-$.innerShInviteList = [
+$.innerShInviteList = ['H8mphLbwLg2ieoefF9Vhh_StunJysWo7'
 ];
 if ($.isNode()) {
   Object.keys(jdCookieNode).forEach((item) => {
