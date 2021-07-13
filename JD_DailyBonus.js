95f349883ad84af2bc33dd9931f1d96a25fe29cf
yuannian1112 <84368132+yuannian1112@users.noreply.github.com>
2021-07-13 23:12:03 +0800

    Update JD_DailyBonus.js

--- JD_DailyBonus.js
+++ JD_DailyBonus.js
@@ -29,8 +29,8 @@ var out = 0; //接口超时退出, 用于可能发生的网络不稳定, 0则关
 
 var $nobyda = nobyda();
 
-const zooFaker = require('./JDJRValidator_Pure')
-const Faker = require('./JDSignValidator')
+const zooFaker = require('./JDJRValidator_Pure.js')
+const Faker = require('./JDSignValidator.js')
 let fp = '', eid = ''
 
 $nobyda.get = zooFaker.injectToRequest2($nobyda.get.bind($nobyda), 'channelSign')
