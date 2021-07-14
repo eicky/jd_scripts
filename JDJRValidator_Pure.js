ad1b909b504b5204ca9d4510bbc86d6c98f0221a
yuannian1112 <84368132+yuannian1112@users.noreply.github.com>
2021-07-15 01:22:37 +0800

    Update JDJRValidator_Pure.js

--- JDJRValidator_Pure.js
+++ JDJRValidator_Pure.js
@@ -8,7 +8,7 @@ const stream = require('stream');
 const zlib = require('zlib');
 const vm = require('vm');
 const PNG = require('png-js');
-const UA = require('../USER_AGENTS.js').USER_AGENT;
+const UA = require('./USER_AGENTS.js').USER_AGENT;
 
 
 Math.avg = function average() {
