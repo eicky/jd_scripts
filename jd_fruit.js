a4d96999dc4fe2362fa8e56c2329d5515b4a47ed
eicky <172308834@qq.com>
2021-10-02 22:12:39 +0800

    update

--- jd_fruit.js
+++ jd_fruit.js
@@ -32,7 +32,6 @@ let cookiesArr = [], cookie = '', jdFruitShareArr = [], isBox = false, notify, n
 //此此内容是IOS用户下载脚本到本地使用，填写互助码的地方，同一京东账号的好友互助码请使用@符号隔开。
 //下面给出两个账号的填写示例（iOS只支持2个京东账号）
 let shareCodes = [ // 这个列表填入你要助力的好友的shareCode
-<<<<<<< HEAD
   ''
 ]
 let message = '', subTitle = '', option = {}, isFruitFinished = false;
