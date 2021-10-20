95f73a05ee09b483eb6f2bea77a0c43263c14b9b
Eicky <eickywl@gmail.com>
2021-10-20 14:22:48 +0800

    删除城分现金助力码

--- jd_city.js
+++ jd_city.js
@@ -33,7 +33,7 @@ if ($.isNode()) {
   cookiesArr = [$.getdata('CookieJD'), $.getdata('CookieJD2'), ...jsonParse($.getdata('CookiesJD') || "[]").map(item => item.cookie)].filter(item => !!item);
 }
 const JD_API_HOST = 'https://api.m.jd.com/client.action';
-let inviteCodes = ['RtGKzeX2SQigedLMFddn1cwQcppajCK5h_oaMK09uxa1GVIdcQ@W9GzoJjSI0PrL-PvVLx_mjHeuJbxWx_0uScsLnKcvJ3BSyQ']
+let inviteCodes = []
 !(async () => {
   if (!cookiesArr[0]) {
     $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', {"open-url": "https://bean.m.jd.com/bean/signIndex.action"});
